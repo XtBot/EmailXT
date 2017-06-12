b302122dafbd4d3d809b1b5df1d522a2e17b4492
roboxt <robo.xt7aj@gmail.com>
2017-06-13 00:59:32 +0430

    Create bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,8 @@ r = redis.Redis('localhost')
 from tempmail import TempMail
 token = '282072697:AAHOF0Bfj7WsB47bTfemGfyQHUYf2AKNu74'
 bot = telebot.TeleBot(token)
-user = @bot.message_handler(commands=['start'])
+user = bot.get_me().username
+@bot.message_handler(commands=['start'])
 def start(m):
   # start text
   text = 'Hi.\nWelcome to {}.\nCommands: \n/newmail: For making a new Email.\n/mails: For reading new emails.'.format(user)
